f9921479ca932482232e97f4d82b0e05dcd35463
Ross Allan <ross@nallar.me>
2016-02-06 15:15:24 +0000

    Add method cloning support

--- src/main/java/me/nallar/javapatcher/patcher/Patches.java
+++ src/main/java/me/nallar/javapatcher/patcher/Patches.java
@@ -1105,6 +1105,15 @@ public class Patches {
 		ctBehavior.setModifiers(ctBehavior.getModifiers() & ~Modifier.SYNCHRONIZED);
 	}
 
+	@Patch(requiredAttributes = "from,to")
+	public void clone(CtMethod method, Map<String, String> attributes) throws CannotCompileException {
+		String from = attributes.get("from");
+		String to = attributes.get("to");
+		CtMethod copy = CtNewMethod.copy(method, method.getDeclaringClass(), null);
+		copy.setName(to);
+		method.getDeclaringClass().addMethod(copy);
+	}
+
 	private void synchronize(CtMethod ctMethod, String field) throws CannotCompileException {
 		if (field == null) {
 			int currentModifiers = ctMethod.getModifiers();
