31a523d96ba66831f794d89efda2c8e37c94698e
Ross Allan <ross@nallar.me>
2016-02-29 23:59:16 +0000

    Use javadoc, not block comment

--- src/main/java/me/nallar/javapatcher/patcher/Patch.java
+++ src/main/java/me/nallar/javapatcher/patcher/Patch.java
@@ -14,7 +14,9 @@ public @interface Patch {
 	// Required attributes, comma delimited. Default "" = none
 	String requiredAttributes() default "";
 
-	// If the constructor should be passed if no method is given and ambiguous parameters
-	// Else, gives class
+	/**
+	 * If the constructor should be passed if no method is given and ambiguous parameters
+	 * Else, gives class
+	 */
 	boolean emptyConstructor() default true;
 }
